d5e6e7e702e1ee169fec0037c7054f2e9dd022e6
maozy <am_stronager@163.com>
2019-06-20 00:16:07 +0800

    test

--- src/main/java/com/mao/demo/MyDemo.java
+++ src/main/java/com/mao/demo/MyDemo.java
@@ -4,9 +4,7 @@ public class MyDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("demo!");
-		System.out.println("papa");
-		System.out.println("nihao");
+		System.out.println("test!");
 	}
 
 }
