039e447e6b3cda587f93fdaea50d2a7a42f82332
maozy <am_stronager@163.com>
2019-06-20 00:22:48 +0800

    重新提交

--- src/main/java/com/mao/demo/MyDemo.java
+++ src/main/java/com/mao/demo/MyDemo.java
@@ -5,6 +5,8 @@ public class MyDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test!");
+		System.out.println("demo!");
+		System.out.println("tata");
 	}
 
 }
