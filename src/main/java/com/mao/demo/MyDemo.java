5536c13a165950e0de96cddad5eb3d7ee7367c21
maozy <am_stronager@163.com>
2019-06-20 00:00:11 +0800

    改成塔塔

--- src/main/java/com/mao/demo/MyDemo.java
+++ src/main/java/com/mao/demo/MyDemo.java
@@ -5,7 +5,7 @@ public class MyDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("demo!");
-		System.out.println("tutu");
+		System.out.println("tata");
 	}
 
 }
